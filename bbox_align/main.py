8fcdaabdbc8c247e77f62802d0031c5816bcef8c
groot <gauthamv93@yahoo.com>
2025-06-29 17:12:16 +0530

    validations refactor

--- bbox_align/main.py
+++ bbox_align/main.py
@@ -1,6 +1,7 @@
 import statistics
 from itertools import combinations
 
+from .validations import check_user_inputs
 from typing import List, Tuple, Set
 from .types import Point, Number, GeometryLine
 from .bounding_box import BoundingBox
@@ -159,36 +160,14 @@ def get_lines(
 
 def process_with_meta_info(
     bounding_boxes: BBoxes,
-    boundaries: List[Tuple[Number, Number]],
+    boundaries: BBox,
     allow_overlaps: bool = False,
 ) -> Tuple[Lines, InLines, PassThroughs, PointOfIntersections]:
 
-    ######################### Check user inputs ############################
-    if not isinstance(bounding_boxes, list) or not all(
-        isinstance(bbox, list) and len(bbox) == 4 for bbox in bounding_boxes
-    ):
-        raise ValueError("bounding_boxes must be a list of 4-point lists.")
-
-    if not isinstance(boundaries, list) or not len(boundaries) == 4:
-        raise ValueError("boundaries must be a list of 4-points")
-
-    # Validate the orientation of each bounding box
-    for idx, bbox in enumerate(bounding_boxes):
-        try:
-            p1, p2, p3, p4 = bbox
-            if not (Point(*p1).is_left_of(Point(*p2)) and
-                    Point(*p2).is_above(Point(*p3)) and
-                    Point(*p3).is_right_of(Point(*p4)) and
-                    Point(*p4).is_below(Point(*p1))):
-                raise ValueError(
-                    f"Bounding box at index {idx} has invalid orientation. "
-                    f"Points must be in clockwise order: "
-                    f"top-left (p1), top-right (p2), bottom-right (p3), bottom-left (p4). "
-                    f"Provided points: p1={p1}, p2={p2}, p3={p3}, p4={p4}"
-                )
-        except ValueError as e:
-            raise ValueError(f"Input validation error: {e}")
-    # ######################################################################
+    try:
+        check_user_inputs(bounding_boxes, boundaries)
+    except ValueError as e:
+        raise ValueError(f"Input Validation: {e}")
 
     bboxes = [
         to_bbox_object(vertex, idx)
